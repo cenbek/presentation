5a3f0f9891632fe8203d3dd9a0818ceb4b454bc7
bilsa <developer@bilsadeveloper.(none)>
2013-02-05 13:35:47 +0200

    initialize the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Presentation::Application.config.secret_token = '15c8c6c8e3e3dc4855418740cca011fdedcc8bfb0f465201f166310b985e7da0273c166191f3535dd8355294259aa77f95e80858a58a8a42d9896186461717f6'
