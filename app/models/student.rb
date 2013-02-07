5be24100d5c156d3dc3991568eac1f292c4a54f6
Tozkoparan <cetintozkoparan@gmail.com>
2013-02-07 17:36:04 +0200

    models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ActiveRecord::Base
   attr_accessible :sclass, :name, :number, :status, :surname
+  validates :name, presence:   true, length: { maximum: 50 }
+  validates :surname, presence:   true, length: { maximum: 50 }
 end
